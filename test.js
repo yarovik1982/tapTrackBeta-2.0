f19dbc2680bf5d21d7e1707effe1a1d09558d6d6
acunamatata82 <yar.oleg1982@mail.ru>
2024-07-09 11:59:37 +0300

    del json-server & folder data

--- test.js
+++ test.js
@@ -8,35 +8,39 @@
 
 
 
-const link = 'http://localhost:5173/Hoppy_Machine/B_is_for_blueberry_111'
-const linkk = '/Selfmade Brewery/BestBeer_115'
+// const link = 'http://localhost:5173/Hoppy_Machine/B_is_for_blueberry_111'
+// const linkk = '/Selfmade Brewery/BestBeer_115'
 
-function decompositionLink(link){
-  const arr = link.split("/").reverse()
-  const first = arr[0]
-  const id = parseInt(first.split('_').slice(-1).join())
-  const beerName = first
-  const breweryName = arr[1]
-  // const beerName = first.split('_').slice(0, -1).join('_')
-  // const breweryName = arr[1].split('_').join('_')
-  return {id:id, breweryName:breweryName, beerName:beerName}
-  // return '/:' + breweryName + '/:' + beerName + '/:_' + id
-}
-const route = decompositionLink(link);
-console.log(route);
+// function decompositionLink(link){
+//   const arr = link.split("/").reverse()
+//   const first = arr[0]
+//   const id = parseInt(first.split('_').slice(-1).join())
+//   const beerName = first
+//   const breweryName = arr[1]
+//   // const beerName = first.split('_').slice(0, -1).join('_')
+//   // const breweryName = arr[1].split('_').join('_')
+//   return {id:id, breweryName:breweryName, beerName:beerName}
+//   // return '/:' + breweryName + '/:' + beerName + '/:_' + id
+// }
+// const route = decompositionLink(link);
+// console.log(route);
 
-function getIdAtLink(link){
-  return parseInt(link.split("_").slice(-1).join())
-}
-const id = getIdAtLink(linkk)
-console.log(typeof id);
+// function getIdAtLink(link){
+//   return parseInt(link.split("_").slice(-1).join())
+// }
+// const id = getIdAtLink(linkk)
+// console.log(typeof id);
 
-//  let div = document.querySelector('div')
- let number = 300;       
- let pos = number + 'px'                                
-//  div.style.top = pos                           
- console.log(pos);          
+// //  let div = document.querySelector('div')
+//  let number = 300;       
+//  let pos = number + 'px'                                
+// //  div.style.top = pos                           
+//  console.log(pos);          
  
 //  const getIdAtLink
 //  const funcion reduce container element class block box main header success error lower case filter sort slice replace includes
 //  container flex item element function class placeholder input button div warning praimary justify content center end map document query selector fragment figure picrure image folder file margin padding info danger sucessfull toaster notification revolution animatiom 
+
+
+
+const str = " \/* string \*/ ";
